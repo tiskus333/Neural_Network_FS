3dca6ee30639bc8dead304e26abe951fc03a01b4
Mateusz Chrusciel <tiskus.3009@gmail.com>
2021-01-24 16:46:13 +0100

    it fucking works, well maybe xD

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class DenseLayer:
     def __init__(self, n_inputs, n_neurons,activation_function = "relu"):
@@ -40,11 +41,15 @@ class Softmax:
         """Compute the softmax of vector x."""
         # exps = np.exp(inputs - inputs.max())
         # return exps / np.sum(exps)
-        inputs -= np.max(inputs)
-        prob = (np.exp(inputs).T / np.sum(np.exp(inputs), axis=1)).T
+        z = np.exp(inputs - np.max(inputs, axis=-1, keepdims=True))
+        prob = z / np.sum(z, axis=-1,keepdims=True)
         return prob
     def backward(self,inputs):
-        return  inputs*(inputs -(inputs * inputs).sum(axis=1)[:,None])
+        tmp = self.forward(inputs)
+        grad_coeff = np.zeros_like(tmp)
+        # grad_coeff[np.arange(inputs.shape[0]), y] = -1
+        grad_coeff += tmp
+        return grad_coeff
 
 def MSE(outputs, targets):
     return (np.square(np.subtract(targets,outputs))).mean(axis=1)
@@ -74,12 +79,11 @@ class NeuralNetwork:
                 target_batch = targets[j:j+batch_size]
                 out = self.forward_propagate(input_batch)
                 error = cross_entropy_loss(out,target_batch)
-                loss = error.mean()
-                l.append(loss)
-                self.back_propagate(input_batch,error)
-                self.gradient_descent(learning_rate)
+                l.append(error)
+                self.back_propagate(input_batch,out-target_batch,error,learning_rate)
+                # self.gradient_descent(learning_rate)
             if (i+1)%10 == 0:
-                print("epochs:", i + 1, "==== error:", loss)  
+                print("epochs:", i + 1, "==== error:", np.average(l))  
 
 
     def test(self,test_data):
@@ -94,48 +98,61 @@ class NeuralNetwork:
             tmp_data = layer.forward(tmp_data)
         return tmp_data
 
-    def back_propagate(self,inputs,error):
+    def back_propagate(self,inputs,delta,error,learning_rate):
         layer2_error = error
-        layer2_delta = layer2_error * self.layers[1].activation_function.backward(self.layers[1].activations)
+        # layer2_delta = layer2_error * self.layers[1].activations
+        layer2_delta = layer2_error * delta
         self.layers[1].derivatives = np.dot(self.layers[0].activations.T,layer2_delta)
-
+        self.layers[1].biases -= learning_rate * np.sum(layer2_delta,axis=0)
+        self.layers[1].weights -= learning_rate * self.layers[1].derivatives
 
         layer1_error = np.dot(layer2_delta,self.layers[1].weights.T)
         layer1_delta = layer1_error * self.layers[0].activation_function.backward(self.layers[0].activations)
         self.layers[0].derivatives = np.dot(inputs.T,layer1_delta)
-
+        self.layers[0].biases -= learning_rate * np.sum(layer1_delta,axis=0)
+        self.layers[0].weights -= learning_rate * self.layers[0].derivatives
         # for layer in reversed(self.layers):
         #     activation = layer.activations
         #     delta = error * layer.activation_function.backward(activation)
-        #     layer.derivatives = np.dot(activation,delta)
+        #     layer.derivatives = np.dot(activation.T,delta)
         #     error = np.dot(delta,layer.weights.T)
         # return error
 
     def gradient_descent(self,learning_rate):
         for layer in self.layers:
-            #layer.biases -= layer.derivatives
+            layer.biases -= layer.derivatives
             layer.weights -= layer.derivatives*learning_rate
 
 # test1 = np.random.rand(3000,2)
 # test1 /= 2
 # targets = np.array([[i[0]+i[1]] for i in test1])
-test2 = np.random.randint(0,2,(1000,4))
-target2 = np.zeros((1000,2))
-for x,y in zip(test2,target2):
-    if x[0] + x[2] == 2 and x[1] == 0:
-        y[1] = 1
-    else:
-        y[0] = 1
+np.random.seed(1337)
+
+blue = np.random.randn(700, 2) + np.array([0, -3])
+pink = np.random.randn(700, 2) + np.array([3, 3])
+yellow = np.random.randn(700, 2) + np.array([-3, 3])
+
+feature_set = np.vstack([blue, pink, yellow])
+
+labels = np.array([0]*700 + [1]*700 + [2]*700)
+
+one_hot_labels = np.zeros((2100, 3))
+
+for i in range(2100):
+    one_hot_labels[i, labels[i]] = 1
+
+plt.figure(figsize=(10,7))
+plt.scatter(feature_set[:,0], feature_set[:,1], c=labels, cmap='plasma', s=100, alpha=0.5)
+plt.show()
 
 
-nn = NeuralNetwork(4,[3],2,"sigmoid")
-nn.train(test2,target2,100,10,0.1)
+nn = NeuralNetwork(2,[4],3,"relu")
+nn.train(feature_set,one_hot_labels,100,10,0.001)
 print("RESULTS: ")
-print(nn.predict([0,1,1,0]))
-print(nn.predict([0,0,1,0]))
-print(nn.predict([0,1,1,1]))
-print(nn.predict([1,1,1,1]))
-print(nn.predict([0,0,0,0]))
+print(nn.predict([0,-3]))
+print(nn.predict([2,2]))
+print(nn.predict([-2,3]))
+
 
 
 # nn.train(test1,targets,100,0.1)
